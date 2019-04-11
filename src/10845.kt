1bbbc6b9c66ef9a90ca1ed46c366db5901edbe3e
정현성 <jin55789@naver.com>
2019-04-11 13:50:00 +0900

    10845. 큐 주석추가

--- src/10845.kt
+++ src/10845.kt
@@ -3,50 +3,49 @@ import java.util.*
 
 val queue: Queue<Int> = LinkedList<Int>()
 
+
 fun main(args: Array<String>) {
+
     val sc = Scanner(System.`in`)
 
     val n = sc.nextInt()
 
-    if (n in 1..10000) {
+    if(n in 1.. 100000) {
 
-        for (i in 0..n) {
+        for(i in 0.. n) {
 
             val input = sc.nextLine().split(" ")
 
-            when (input[0]) {
+            when(input[0]) {
                 "push" -> push(input[1].toInt())
 
                 "pop" -> pop()
 
-                "front" -> front()
-
-                "back" -> back()
+                "size" -> size()
 
                 "empty" -> empty()
 
-                "size" -> size()
+                "front" -> front()
+
+                "back" -> back()
             }
         }
     }
 }
 
 fun push(value: Int) {
-    queue.offer(value)
+    queue.offer(value)  //Queue에 삽입
 }
 
-fun front() {
+fun pop() {
     if (queue.isEmpty())
         println(-1)
     else
-        println(queue.element())
+        println(queue.poll())    //Queue에서 제거
 }
 
-fun back() {
-    if (queue.isEmpty())
-        println(-1)
-    else
-        println(queue.last())
+fun size() {
+    println(queue.size)
 }
 
 fun empty() {
@@ -56,14 +55,140 @@ fun empty() {
         println(0)
 }
 
-fun size() {
-    println(queue.size)
-}
-
-fun pop() {
-    if(queue.isEmpty())
+fun front() {
+    if (queue.isEmpty())
         println(-1)
     else
-        queue.poll()
+        println(queue.peek())   //Queue에서 가장 앞에 있는 수 읽기}
 }
+
+fun back() {
+    if (queue.isEmpty())
+        println(-1)
+    else
+        println(queue.last())   //Queue에서 가장 뒤에 있는 수 읽기
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//val queue: Queue<Int> = LinkedList<Int>()
+//
+//fun main(args: Array<String>) {
+//    val sc = Scanner(System.`in`)
+//
+//    val n = sc.nextInt()
+//
+//    if (n in 1..10000) {
+//
+//        for (i in 0..n) {
+//
+//            val input = sc.nextLine().split(" ")
+//
+//            when (input[0]) {
+//                "push" -> push(input[1].toInt())
+//
+//                "pop" -> pop()
+//
+//                "front" -> front()
+//
+//                "back" -> back()
+//
+//                "empty" -> empty()
+//
+//                "size" -> size()
+//            }
+//        }
+//    }
+//}
+//
+//fun push(value: Int) {
+//    queue.offer(value)
+//}
+//
+//fun front() {
+//    if (queue.isEmpty())
+//        println(-1)
+//    else
+//        println(queue.element())
+//}
+//
+//fun back() {
+//    if (queue.isEmpty())
+//        println(-1)
+//    else
+//        println(queue.last())
+//}
+//
+//fun empty() {
+//    if (queue.isEmpty())
+//        println(1)
+//    else
+//        println(0)
+//}
+//
+//fun size() {
+//    println(queue.size)
+//}
+//
+//fun pop() {
+//    if(queue.isEmpty())
+//        println(-1)
+//    else
+//        queue.poll()
+//}
 
